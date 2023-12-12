453aebe6bf25a69f4fcaccbb94852a039ff1736f
leosantodev <dossantosleonardo.dev@gmail.com>
2023-12-12 16:35:36 -0300

    insersao de novas moedas na aplicacao

--- javascript.js
+++ javascript.js
@@ -1,32 +1,44 @@
 const convertButton = document.querySelector(".convert-button")
 const currencySelect = document.querySelector(".currency-select")
 
+
 function convertValues(){
     const inputCurrencyValue = document.querySelector(".input-currency").value
-    const currencyValueToVonvert = document.querySelector(".currency-value-to-convert")
-    const currencyValueCnverted = document.querySelector(".currency-value")
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
+    const currencyValueConverted = document.querySelector(".currency-value")
 
 console.log(currencySelect.value)
 
-    const dolarToday = 5.2
-    const euroToday = 6.2
+    const dolarToday = 4.94
+    const euroToday = 5.31
+    const libraToday = 6.21
+    
+ 
 
     
     if(currencySelect.value == "dolar"){
-        currencyValueCnverted.innerHTML = new Intl.NumberFormat("en-US", {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
             style: "currency",
             currency: "USD"
         }).format(inputCurrencyValue / dolarToday)
     }
 
     if(currencySelect.value == "euro"){
-        currencyValueCnverted.innerHTML = new Intl.NumberFormat("de-DE", {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
             style: "currency",
             currency: "EUR"
         }).format(inputCurrencyValue / euroToday)
     }
 
-    currencyValueToVonvert.innerHTML = new Intl.NumberFormat("pr-BR", {
+    if(currencySelect.value == "libra"){
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-GB", {
+            style: "currency",
+            currency: "GBP"
+        }).format(inputCurrencyValue / libraToday)
+    }
+
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency", 
         currency: "BRL"
     }).format(inputCurrencyValue)
@@ -34,20 +46,36 @@ console.log(currencySelect.value)
 }
 
 function changeCurrency(){
-    const currencyName = document.getElementById("currency-name")
+    const currencyName = document.getElementById("currency-name",)
     const currencyImage = document.querySelector(".currency-img")
-    
+        
 
     if(currencySelect.value == "dolar") {
         currencyName.innerHTML = "Dolar"
         currencyImage.src = "./assets/dollar.png"
+   
     }
 
     if(currencySelect.value == "euro") {
         currencyName.innerHTML = "Euro"
         currencyImage.src = "./assets/euro.png"
+      
     }
 
+    if(currencySelect.value == "libra") {
+        currencyName.innerHTML = "Libra"
+        currencyImage.src = "./assets/libra.png"
+      
+    }
+
+
+    if(currencySelect.value == "real") {
+        currencyName.innerHTML = "Real"
+        currencyImage.src = "./assets/real.png"
+        
+    }
+
+   
     convertValues()
 }
 
